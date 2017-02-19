748ef6e0b89aab07d70091276021ac932d158848
uu59 <k@uu59.org>
2017-02-19 19:25:04 +0900

    Avoid CI fail: command ./test/test_digdag_ui.bash took more than 10 minutes since last output

--- cookbooks/digdag-ui/default.rb
+++ cookbooks/digdag-ui/default.rb
@@ -17,22 +17,47 @@ directory root
 
 run_command <<-SH
   set -e
-  [ -d "#{rev_dir}" ] || {
-    [ -d "#{repo}" ] || git clone https://github.com/treasure-data/digdag #{repo}
-    cd "#{repo}"
-    if ! git rev-list "#{rev}"; then
-      git fetch --all --prune --tags
-    fi
-    rev="$(git rev-list #{rev} | head -n1)"
-    git checkout -f $rev
 
-    cd ./digdag-ui
+  [ -d "#{repo}" ] || git clone -q https://github.com/treasure-data/digdag #{repo}
+SH
+
+run_command <<-SH
+  set -e
+
+  cd "#{repo}"
+  if ! git rev-list "#{rev}"; then
+    git fetch --all --prune --tags
+  fi
+  rev="$(git rev-list #{rev} | head -n1)"
+  git checkout -f $rev
+SH
+
+unless File.directory?(rev_dir)
+  run_command <<-SH
+    set -e
+    cd "#{repo}/digdag-ui"
+    orig_set="$(npm config get progress)"
+
+    npm set progress=false
     npm i
-    npm run build
+    npm set progress=$orig_set
+  SH
 
+  run_command <<-SH
+    set -e
+    cd "#{repo}/digdag-ui"
+    npm run build
+  SH
+end
+
+run_command <<-SH
+  set -e
+
+  [ -d "#{rev_dir}" ] || {
     mkdir -p "#{rev_dir}"
-    cp -a ./public/* "#{rev_dir}/"
+    cp -a "#{repo}/digdag-ui/public/"* "#{rev_dir}/"
   }
+
   ln -sfn "#{rev_dir}" #{root}/current
   chown -R #{node[:core][:owner]}:#{node[:core][:group]} "#{rev_dir}"
   chmod -R +r "#{rev_dir}"
