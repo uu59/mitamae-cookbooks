84b997a50abe19bf46966cbae03c07301ba54b3f
uu59 <k@uu59.org>
2017-02-19 18:46:39 +0900

    digdag-ui: properly setup

--- cookbooks/digdag-ui/default.rb
+++ cookbooks/digdag-ui/default.rb
@@ -3,20 +3,48 @@ include_recipe "../git/default.rb"
 
 node.reverse_merge!({
   "digdag-ui" => {
-    "repo" => "/opt/digdag",
+    "root" => "/opt/digdag-ui",
     "revision" => "v0.9.5",
   }
 })
 
-repo = node["digdag-ui"][:repo]
+root = node["digdag-ui"][:root]
+repo = "#{root}/repo"
 rev = node["digdag-ui"][:revision]
+rev_dir = "#{root}/#{rev}"
+
+directory root
 
 run_command <<-SH
   set -e
-  git clone https://github.com/treasure-data/digdag #{repo}
-  cd #{repo}
-  git checkout -f #{rev}
-  cd ./digdag-ui
-  npm i
-  npm run build
+  [ -d "#{rev_dir}" ] || {
+    [ -d "#{repo}" ] || git clone https://github.com/treasure-data/digdag #{repo}
+    cd "#{repo}"
+    if ! git rev-list "#{rev}"; then
+      git fetch --all --prune --tags
+    fi
+    rev="$(git rev-list #{rev} | head -n1)"
+    git checkout -f $rev
+
+    cd ./digdag-ui
+    npm i
+    npm run build
+
+    mkdir -p "#{rev_dir}"
+    cp -a ./public/* "#{rev_dir}/"
+  }
+  ln -sfn "#{rev_dir}" #{root}/current
+  chown -R #{node[:core][:owner]}:#{node[:core][:group]} "#{rev_dir}"
+  chmod -R +r "#{rev_dir}"
+SH
+
+verify <<-SH
+  test -f "#{rev_dir}/index.html"
+  test -L "#{root}/current"
+  test "$(readlink -e "#{root}/current")" = "#{rev_dir}"
+  test "$(stat -c "%U:%G" "#{rev_dir}")" = "#{node[:core][:owner]}:#{node[:core][:group]}"
+
+  find "#{rev_dir}" | while read file; do
+    test "$(stat -c "%U:%G" "$file")" = "#{node[:core][:owner]}:#{node[:core][:group]}"
+  done
 SH
