9ff1370f2eb5fb850c5d62a57fc7ac196226be3b
uu59 <k@uu59.org>
2017-02-19 22:17:11 +0900

    core: human-friendly display for verify

--- cookbooks/core/verify.rb
+++ cookbooks/core/verify.rb
@@ -1,6 +1,14 @@
 define :verify do
-  run_command <<-SH
-    set -uex
-    #{params[:name]}
-  SH
+  # /mitamae/repo/cookbooks/core/default.rb:24:in MItamae::Recipe#instance_eval
+  # to
+  # ./cookbooks/core/default.rb:24
+  source = caller.grep(/cookbooks/).first.gsub(/.*?cookbooks/, "./cookbooks").gsub(/:in.*/, "")
+
+  execute "verify from #{source}" do
+    action :run
+    command <<-SH
+      set -uex
+      #{params[:name]}
+    SH
+  end
 end
